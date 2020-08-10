29b02630e8018768e96122b44685483a35319a8b
resumurof <santhosh10000@gmail.com>
2020-08-09 20:35:43 -0400

    Add files via upload
    JQuery Concepts with example.

---
+++ Jquery/01/01/GetHelpText.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="GetHelpText.aspx.cs" Inherits="_01.GetHelpText" %>
+
+<div id="divResult" runat="server"></div>
---
+++ Jquery/01/01/GetHelpText.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+namespace _01
+{
+    public partial class GetHelpText : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string parameter = Request["HelpTextKey"];
+           divResult.InnerText = GetHelpTextByKey(parameter);
+        }
+
+        private string GetHelpTextByKey(string key)
+        {
+            string helpText = string.Empty;
+
+            string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlCommand cmd = new SqlCommand("spGetHelpTextByKey", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlParameter parameter = new SqlParameter("@HelpTextKey", key);
+                cmd.Parameters.Add(parameter);
+                con.Open();
+                helpText = cmd.ExecuteScalar().ToString();
+            }
+
+            return helpText;
+        
+        }
+
+    }
+}
